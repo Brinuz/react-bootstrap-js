bbe7f59e091081752b878261e11ff8d7ca41b859
Bruno Monteiro <brinuz@gmail.com>
2020-01-29 16:36:44 +0000

    Add quote linting error

--- src/App/app.test.jsx
+++ src/App/app.test.jsx
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react';
-import * as React from 'react';
-import App from './app';
+import { render } from "@testing-library/react";
+import * as React from "react";
+import App from "./app";
 
-it('renders the app', () => {
+it("renders the app", () => {
     const document = render(<App />);
     expect(document).toBeTruthy();
 });
